477ed2c2b46f8ad3db79e893e3b4c389b02ff832
iGFarr <isaacgfarr@live.com>
2022-07-01 23:53:25 -0500

    Add Cube Buddy V2

--- CubeBuddyProgUI/CubeBuddyProgUI/CBBaseViewController.swift
+++ CubeBuddyProgUI/CubeBuddyProgUI/CBBaseViewController.swift
@@ -12,6 +12,10 @@ class CBBaseViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor(named: "CBPrimary")
+        if let nav = self.navigationController {
+            nav.navigationBar.barTintColor = UIColor(named: "CBSecondary")
+        }
     }
 
 
